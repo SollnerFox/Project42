b64ea7525c14e04766d3484acc324fdc0aba041c
SollnerFox <milan.crusnik.02@gmail.com>
2024-11-25 00:47:59 +0200

    Updated movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,22 +3,19 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class PlayerController : MonoBehaviour
 {
-    //[SerializeField] private Rigidbody _rb;
     [SerializeField] private float _speed = 5;
-    [SerializeField] private float _turnSpeed = 360;
     
-    private CharacterController _controller;
-    private Camera _mainCamera;
-    private Plane _groundPlane;
 
     private float _currentAttractionCharacter = 0f;
     private float _gravityForce = 20f;
+
+    private CharacterController _controller;
+    private Camera _mainCamera;
 
     private void Awake()
     {
         _controller = GetComponent<CharacterController>();
         _mainCamera = Camera.main;
-        _groundPlane = new Plane(Vector3.up, Vector3.zero);
     }
 
     private void Update()
@@ -29,28 +26,15 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        GravityHadling();
+        GravityHandling();
     }
 
     private void Look()
     {
         Ray ray = _mainCamera.ScreenPointToRay(Input.mousePosition);
-        Debug.DrawRay(ray.origin, ray.direction * 100, Color.red);
-        /*if (_groundPlane.Raycast(ray, out float enter))
-        {
-            Vector3 targetPoint = ray.GetPoint(enter);
-            Vector3 direction = targetPoint - transform.position;
-            direction.y = 0;
-
-            if (direction != Vector3.zero)
-            {
-                Quaternion targetRotation = Quaternion.LookRotation(direction.ToIso(), Vector3.up);
-                transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _turnSpeed * Time.deltaTime);
-            }
-        }*/
         if (Physics.Raycast(ray, out RaycastHit hit))
         {
-            Vector3 lookDirection = new Vector3(hit.point.x - transform.position.x, 0, hit.point.z - transform.position.z);
+            Vector3 lookDirection = new Vector3(hit.point.x, transform.position.y, hit.point.z);
             transform.LookAt(lookDirection);
         }
     }
@@ -58,20 +42,14 @@ public class PlayerController : MonoBehaviour
     private void Move()
     {
         Vector3 moveDirection =
-            (transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")) *
-            _speed * Time.deltaTime + Vector3.up * _currentAttractionCharacter * Time.deltaTime;
+            ((transform.forward * Input.GetAxisRaw("Vertical") + transform.right * Input.GetAxisRaw("Horizontal")) *
+            _speed  + Vector3.up * _currentAttractionCharacter ) * Time.deltaTime;
         _controller.Move(moveDirection);
     }
 
-    private void GravityHadling()
+    private void GravityHandling()
     {
         if (!_controller.isGrounded) _currentAttractionCharacter -= _gravityForce * Time.deltaTime;
         else _currentAttractionCharacter = 0f;
     }
-}
-
-public static class Helpers
-{
-    private static Matrix4x4 _isoMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 45, 0));
-    public static Vector3 ToIso(this Vector3 input) => _isoMatrix.MultiplyPoint3x4(input);
 }
